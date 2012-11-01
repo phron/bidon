d44abad814b8db190aea8cec0358b69f6f278abf
asdemoulin <phronexia@gmail.com>
2012-11-01 17:51:43 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidon::Application.config.secret_token = '6a36c41f8dfa0bdfefeca42da63a62d45cd96a6c05bf7229af258a1195959761c91dac94032c1be4ba804222cf74d72297a5d5f03d287915a87bc18b8e820327'
